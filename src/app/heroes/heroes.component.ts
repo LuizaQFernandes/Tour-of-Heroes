aa455520b49bcb0517be58ce7d6204cce454cd1f
Luiza Fernandes <luhqfernandes@gmail.com>
2021-04-27 09:13:20 -0300

    List of heroes added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +10,22 @@ import { Hero } from '../hero'
 
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-
+ 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
